7f9dfd83f0b25d758ca4f07ae0c92a9c858c40f7
Jason <jasonmoramora@outlook.com>
2019-06-09 17:07:17 -0500

    Second commit

---
+++ routes/bd.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: "localhost",  
+    user: "root",
+    password: "jm739721041307",
+    port: 3306,
+    database:"tienda"
+});
+
+conn.connect(function(err){
+    if(err) throw err;
+    console.log('Conectado con mysql');
+});
+
+module.exports = conn;
